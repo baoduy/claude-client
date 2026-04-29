b758eca8735961ea749973918fa3cdf90bee95aa
Steven Hoang <drunkcoding@outlook.com>
2026-04-29 15:59:50 +0800

    feat(unified): add barrel re-exporting types, events, errors

---
+++ src/unified/index.ts
@@ -0,0 +1,16 @@
+export type {
+  UnifiedStatus,
+  TurnSnapshot,
+  TurnToolUse,
+  TurnToolResult,
+  SendInput,
+  ContentBlock,
+  ImageSource,
+  AICliCapabilities,
+  PermissionMode,
+  SupportedModelsResponse,
+} from './types.js';
+
+export type { UnifiedEventMap, UnifiedEventName } from './events.js';
+
+export { UnsupportedContentError } from './errors.js';
